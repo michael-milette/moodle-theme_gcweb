731117584f07ebd8fa25b5bf483cf953a2688eb9
Michael Milette <michael.milette@tngconsulting.ca>
2022-04-02 09:34:59 -0400

    Updated to version date 2022-04-01.

--- version.php
+++ version.php
@@ -27,7 +27,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 // This is the version of the plugin.
-$plugin->version = 2022030200;
+$plugin->version = 2022040100;
 
 // This is the version of Moodle this plugin requires.
 $plugin->requires = 2020061500; // Moodle 3.9.
